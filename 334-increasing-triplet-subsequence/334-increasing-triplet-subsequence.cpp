73a53990a5d890a6054ff1bcf9549dc0ed1ea5ef
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-01-29 21:02:46 +0530

    Time: 79 ms (51.59%), Space: 61.5 MB (68.89%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -1,38 +1,20 @@
 class Solution {
-    public int lower_bound(ArrayList<Integer> lis,int num){
-        if(lis.size() == 0) return 0;
-        int ans = 0;
-        int low = 0,high = lis.size() - 1;
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            if(lis.get(mid) > num){
-                high = mid - 1;
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n = nums.size();
+        int first = INT_MAX,second = first;
+        
+        for(int i=0;i<n;i++){
+            if(nums[i] <= first){
+                first = nums[i];
+            }
+            else if(nums[i] <= second){
+                second = nums[i];
             }
             else {
-                ans = mid;
-                low = mid + 1;
+                return true;
             }
-        }
-        
-        if(ans < lis.size() && lis.get(ans) < num){
-            ans++;
-        }
-        return ans;
-    }
-    public boolean increasingTriplet(int[] nums) {
-        
-        ArrayList<Integer> lis = new ArrayList<Integer>();
-        
-        for(int e:nums){
-            int index = lower_bound(lis,e);
-            if(index == lis.size()){
-                lis.add(e);
-            }
-            else {
-                lis.set(index,e);
-            }
-            if(lis.size() == 3) return true;
         }
         return false;
     }
-}
+};
