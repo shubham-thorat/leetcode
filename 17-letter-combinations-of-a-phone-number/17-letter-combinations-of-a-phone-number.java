e74d316489d8444cf094dd18f97c6d4589ef4f6e
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-02-04 18:42:45 +0530

    Time: 0 ms (100.00%), Space: 41 MB (10.29%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,36 @@
+class Solution {
+    private void possibleComb(HashMap<Integer,String> numberletter,String digits,int index,List<String> result,StringBuilder sb){
+        if(index == digits.length()){
+            if(sb.length() != 0)
+                result.add(new String(sb.toString()));
+            return;
+        }
+        
+        int digit = digits.charAt(index) - '0';
+        
+        String str = numberletter.get(digit);
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            sb.append(ch);
+            possibleComb(numberletter,digits,index+1,result,sb);
+            sb.deleteCharAt(index);
+        }
+    }
+    public List<String> letterCombinations(String digits) {
+        HashMap<Integer,String> numberletter = new HashMap<>();
+        
+        numberletter.put(2,"abc");
+        numberletter.put(3,"def");
+        numberletter.put(4,"ghi");
+        numberletter.put(5,"jkl");
+        numberletter.put(6,"mno");
+        numberletter.put(7,"pqrs");
+        numberletter.put(8,"tuv");
+        numberletter.put(9,"wxyz");
+        
+        List<String> result = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        possibleComb(numberletter,digits,0,result,sb);
+        return result;
+    }
+}
