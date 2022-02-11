63f3c0c298106584b4b109e7c2eb9a4508e3030b
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-02-11 17:21:00 +0530

    Time: 4 ms (91.89%), Space: 44.4 MB (12.83%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,17 +1,34 @@
 class Solution {
+    // public int lengthOfLIS(int[] nums) {
+    //     int n = nums.length;
+    //     int dp[] = new int[n];
+    //     int ans = 0;
+    //     for(int i=0;i<n;i++){
+    //         dp[i] = 1;
+    //         for(int j=0;j<i;j++){
+    //             if(nums[j] < nums[i]){
+    //                 dp[i] = Math.max(dp[i],dp[j] + 1);
+    //             }
+    //         }
+    //         ans = Math.max(ans,dp[i]);
+    //     }
+    //     return ans;
+    // }
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int dp[] = new int[n];
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            dp[i] = 1;
-            for(int j=0;j<i;j++){
-                if(nums[j] < nums[i]){
-                    dp[i] = Math.max(dp[i],dp[j] + 1);
-                }
+        int[] tails = new int[nums.length];
+        int size = 0;
+        for (int x : nums) {
+            int i = 0, j = size;
+            while (i != j) {
+                int m = (i + j) / 2;
+                if (tails[m] < x)
+                    i = m + 1;
+                else
+                    j = m;
             }
-            ans = Math.max(ans,dp[i]);
+            tails[i] = x;
+            if (i == size) ++size;
         }
-        return ans;
+        return size;
     }
 }
