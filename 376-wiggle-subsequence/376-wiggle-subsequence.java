f87629afbf4e34462fe87bfbc3cff8d44b1a8440
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-02-11 17:54:03 +0530

    Time: 0 ms (100.00%), Space: 42 MB (6.17%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.java
+++ 376-wiggle-subsequence/376-wiggle-subsequence.java
@@ -1,49 +1,22 @@
-class Solution {
+public class Solution {
     public int wiggleMaxLength(int[] nums) {
-        int n = nums.length;
-        if(n <= 1) return n;
-        int dp[] = new int[n];
-        Arrays.fill(dp,1);
-        int ans = 1;
-        int k = 0;
-        while(k < nums.length - 1 && nums[k] == nums[k+1]) k++;
-        if(k == nums.length - 1) return 1;
-        
-        if(nums[k+1] - nums[k] > 0){
-            for(int i=k+1;i<n;i++){
-                for(int j=0;j<i;j++){
-                    if(dp[j] % 2 == 0){
-                        if(nums[j] > nums[i]){
-                            dp[i] = Math.max(dp[i],dp[j] + 1);
-                        }
-                    }
-                    else {
-                        if(nums[j] < nums[i]){
-                            dp[i] = Math.max(dp[i],dp[j] + 1);
-                        }
-                    }
-                }
-                ans = Math.max(ans,dp[i]);
+        if (nums.length < 2)
+            return nums.length;
+        int[] up = new int[nums.length];
+        int[] down = new int[nums.length];
+        up[0] = down[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up[i] = down[i - 1] + 1;
+                down[i] = down[i - 1];
+            } else if (nums[i] < nums[i - 1]) {
+                down[i] = up[i - 1] + 1;
+                up[i] = up[i - 1];
+            } else {
+                down[i] = down[i - 1];
+                up[i] = up[i - 1];
             }
-            return ans;
         }
-        else {
-            for(int i=k+1;i<n;i++){
-                for(int j=0;j<i;j++){
-                    if(dp[j] % 2 == 0){
-                        if(nums[j] < nums[i]){
-                            dp[i] = Math.max(dp[i],dp[j] + 1);
-                        }
-                    }
-                    else {
-                        if(nums[j] > nums[i]){
-                            dp[i] = Math.max(dp[i],dp[j] + 1);
-                        }
-                    }
-                }
-                ans = Math.max(ans,dp[i]);
-            }
-            return ans;
-        }
+        return Math.max(down[nums.length - 1], up[nums.length - 1]);
     }
 }
