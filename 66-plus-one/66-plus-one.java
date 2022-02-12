9508c796038ecda7fd7f463418784605bbb0bd03
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-02-12 21:44:54 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (6.06%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        ArrayList<Integer> list = new ArrayList<>();
-        int carry = 1;
-        for(int i=digits.length - 1;i>=0;i--){
-            int sum = digits[i] + carry;
-            list.add((sum%10));
-            carry = sum/10;
+      
+        for(int i=digits.length -1;i>=0;i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
         }
         
-        if(carry != 0) list.add(carry);
-        Collections.reverse(list);
-        return list.stream().mapToInt(i -> i).toArray();
+        int newno[] = new int[digits.length + 1];
+        newno[0] = 1;
+        return newno;
     }
 }
