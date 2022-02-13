45f6d1b563c32a38f4b7c4396bc17180ba820c5e
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-02-13 13:06:51 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (23.91%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,30 +1,20 @@
 class Solution {
-    void helper(int i,int nums[],List<List<Integer>> a,ArrayList<Integer> arr){
-        if(i == nums.length){
-            ArrayList<Integer> newnode = new ArrayList<>(arr);
-            a.add(newnode);
+    private void helper(List<List<Integer>> result,int nums[],int index,ArrayList<Integer> set){
+        if(index >= nums.length){
+            result.add(new ArrayList<Integer>(set));
             return;
         }
         
-        helper(i+1,nums,a,arr);
-        arr.add(nums[i]);
-        helper(i+1,nums,a,arr);
-        int index = arr.size() - 1;
-        arr.remove(index);
+         helper(result,nums,index + 1,set);
+        set.add(nums[index]);
+        helper(result,nums,index + 1,set);
+        set.remove(set.size() - 1);
     }
     public List<List<Integer>> subsets(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
         
-        ArrayList<Integer> temp = new ArrayList<>();
-        temp.add(nums[0]);
-        for(int e:nums){
-            if(temp.get(temp.size() - 1) != e){
-                temp.add(e);
-            }
-        }
-        
-        List<List<Integer>> a = new ArrayList<List<Integer>>();
-        ArrayList<Integer> arr = new ArrayList<>();
-        helper(0,temp.stream().mapToInt(e -> e).toArray(),a,arr);       
-        return a;
+        helper(result,nums,0,new ArrayList<Integer>());
+        return result;
     }
 }
