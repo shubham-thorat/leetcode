6ff6eff60f458e021ed267169a73620340fd2e04
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-02-14 11:01:50 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (48.64%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
-    int integerBreak(int n) {
-      vector<int> dp(n + 1, 1);
-        for (int i = 3; i <= n; i++) {
-            for (int j = 1; j < i; j++) {
-                dp[i] = max(dp[i], max({dp[i - j] * dp[j], dp[i - j] * j, (i - j) * j}));
-            }
+    int helper(int n,vector<int> &dp){
+        if(n == 1) return 1;
+        if(n == 2) return 1;
+        if(dp[n] != -1) return dp[n];
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            int opt1 =  max((n - i) *  i, i * helper(n-i,dp));
+            ans = max(opt1,ans);
         }
-        return dp[n];
+        return dp[n] = ans;
+    }
+    int integerBreak(int n) {
+        vector<int> dp(n+1,-1);
+        return helper(n,dp);
     }
 };
