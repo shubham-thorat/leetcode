0de57f667b4b0697e6807d2973a8afe30d5f16e0
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-02-23 13:32:00 +0530

    Time: 43 ms (45.75%), Space: 43 MB (29.86%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -19,29 +19,63 @@ class Node {
 */
 
 class Solution {
-    private void dfs(Node node,Node copy,HashMap<Node,Node> visited){
-        visited.put(node,copy);
-        List<Node> nbr = node.neighbors;
+//     private void dfs(Node node,Node copy,HashMap<Node,Node> visited){
+//         visited.put(node,copy);
+//         List<Node> nbr = node.neighbors;
         
-        for(Node currnode:nbr){
-            if(!visited.containsKey(currnode)){
-                Node neighborcopy = new Node(currnode.val);
+//         for(Node currnode:nbr){
+//             if(!visited.containsKey(currnode)){
+//                 Node neighborcopy = new Node(currnode.val);
                 
-                copy.neighbors.add(neighborcopy);
+//                 copy.neighbors.add(neighborcopy);
                 
-                dfs(currnode,neighborcopy,visited);
-            }
-            else {
-                copy.neighbors.add(visited.get(currnode));
-            }
+//                 dfs(currnode,neighborcopy,visited);
+//             }
+//             else {
+//                 copy.neighbors.add(visited.get(currnode));
+//             }
+//         }
+//     }
+//     public Node cloneGraph(Node node) {
+//         if(node == null) return null;
+//         HashMap<Node,Node> visited = new HashMap<>();
+        
+//         Node copy = new Node(node.val);
+//         dfs(node,copy,visited);
+//         return copy;
+//     }
+    
+    public Node cloneGraph(Node root) {
+      if (root == null) return null;
+
+      // use a queue to help BFS
+      Queue<Node> queue = new LinkedList<Node>();
+      queue.add(root);
+
+      // use a map to save cloned nodes
+      Map<Node, Node> map = new HashMap<Node, Node>();
+
+      // clone the root
+      map.put(root, new Node(root.val));
+
+      while (!queue.isEmpty()) {
+        Node node = queue.poll();
+
+        // handle the neighbors
+        for (Node neighbor : node.neighbors) {
+          if (!map.containsKey(neighbor)) {
+            // clone the neighbor
+            map.put(neighbor, new Node(neighbor.val));
+            // add it to the next level
+            queue.add(neighbor);
+          }
+
+          // copy the neighbor
+          map.get(node).neighbors.add(map.get(neighbor));
         }
+      }
+
+      return map.get(root);
     }
-    public Node cloneGraph(Node node) {
-        if(node == null) return null;
-        HashMap<Node,Node> visited = new HashMap<>();
-        
-        Node copy = new Node(node.val);
-        dfs(node,copy,visited);
-        return copy;
-    }
+    
 }
