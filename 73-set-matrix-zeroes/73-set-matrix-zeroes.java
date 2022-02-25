34d379aa6d88c003181ce4558f313c5adfd22420
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-02-25 11:42:16 +0530

    Time: 1 ms (93.21%), Space: 54.1 MB (21.46%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -3,23 +3,22 @@ class Solution {
         int no_row = matrix.length;
         int no_col = matrix[0].length;
         
-        int clone[][] = new int[no_row][no_col];        
+        int rows[] = new int[no_row];
+        int cols[] = new int[no_col];
+        
         for(int row = 0;row < no_row; row++){
             for(int col=0;col < no_col; col++){
-                clone[row][col] = matrix[row][col];
+                if(matrix[row][col] == 0){
+                    rows[row] = 1;
+                    cols[col] = 1;
+                }
             }
         }
         
-        for(int row = 0;row < no_row;row++){
-            for(int col=0;col < no_col; col++){
-                if(clone[row][col] == 0){
-                    for(int i=0;i<no_row;i++){
-                        matrix[i][col] = 0;    
-                    }
-                    
-                    for(int i=0;i<no_col;i++){
-                        matrix[row][i] = 0;
-                    }
+        for(int row=0;row<no_row;row++){
+            for(int col=0;col<no_col;col++){
+                if(rows[row] == 1 || cols[col] == 1){
+                    matrix[row][col] = 0;
                 }
             }
         }
