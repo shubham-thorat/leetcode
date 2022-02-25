d3df303a0b3e2cb2d921f27b4d1e547fa673a328
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-02-25 12:13:15 +0530

    Time: 1 ms (93.21%), Space: 54.3 MB (17.66%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -2,17 +2,16 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int no_row = matrix.length;
         int no_col = matrix[0].length;
-         
-        boolean isrootcell = false;
-         for(int i=0;i<no_row;i++){
-            if(matrix[i][0] == 0) {
-                isrootcell = true;
-                break;
-            }
-        }
         
-        for(int row = 0;row < no_row; row++){
+        boolean isfirstcol = false,isfirstrow = false;
+        
+         
+        for(int i=0;i<no_col;i++){ if(matrix[0][i] == 0) {isfirstrow = true; break; }}
+        for(int i=0;i<no_row;i++) { if(matrix[i][0] == 0) {isfirstcol = true; break; }}
+        
+        for(int row = 1;row < no_row; row++){
             for(int col=1;col < no_col; col++){
+                
                 if(matrix[row][col] == 0){
                     matrix[row][0] = 0;
                     matrix[0][col] = 0;
@@ -22,24 +21,14 @@ class Solution {
         
         
         for(int row=1;row<no_row;row++){
-            for(int col=1;col<no_col;col++){
+            for(int col=1;col<no_col;col++){                
                 if(matrix[0][col] == 0 || matrix[row][0] == 0){
                     matrix[row][col] = 0;
                 }
             }
         }
         
-        if(matrix[0][0] == 0){
-             for(int i=0;i<no_col;i++){
-                matrix[0][i] = 0;
-            }
-        }
-        
-        if(isrootcell){
-            for(int i=0;i<no_row;i++){
-                matrix[i][0] = 0;
-            }
-            
-        }
+        if(isfirstrow) for(int i=0;i<no_col;i++) matrix[0][i] = 0;
+        if(isfirstcol) for(int i=0;i<no_row;i++) matrix[i][0] = 0;
     }
 }
