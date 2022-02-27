60a03168a3e676c2e6a257fe4e753f520f2b0083
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-02-27 11:54:56 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (44.31%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+       if(n == 0) return 1;
+    if(n ==INT_MIN){
+        x = x * x;
+        n = n/2;
+    }
+    if(n < 0) {
+        n = -n;
+        x = 1/x;
+    }
+    
+    return (n%2 == 0) ? myPow(x * x, n/2) : x *  myPow(x * x, n/2);
     }
 };
