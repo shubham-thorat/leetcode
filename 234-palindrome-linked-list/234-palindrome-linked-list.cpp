e31e2721e98f0ae78635dde3ee687af4488c555f
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-03-03 22:40:18 +0530

    Time: 200 ms (91.15%), Space: 121 MB (38.87%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -9,38 +9,16 @@
  * };
  */
 class Solution {
-
 public:
-    ListNode *findMid(ListNode *head){
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast->next!=NULL&&fast->next->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
-        }  
-        return slow;
+    ListNode *curr;
+    bool check(ListNode *head){
+        if(head == NULL) return true;
+        bool ispal = (check(head->next) && (head->val == curr->val));
+        curr = curr->next;
+        return ispal;
     }
-    ListNode* reverse(ListNode *head){
-        if(head == NULL || head->next == NULL) return head;
-        
-        ListNode *curr = reverse(head->next);
-        head->next->next = head;
-        head->next = NULL;
-        return curr;
-    }
-    
     bool isPalindrome(ListNode* head) {
-        if(head == NULL || head->next == NULL) return true;
-        ListNode *mid = findMid(head);
-        if(mid == NULL) return true;
-        ListNode *second = reverse(mid->next);
-        mid->next = NULL;
-        
-        while(head != NULL && second != NULL){
-            if(head->val != second->val) return false;
-            head = head->next;
-            second = second->next;
-        }
-        return true;
+        curr = head;
+        return check(head);
     }
 };
