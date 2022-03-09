867e76c32e540c07b8b5b952292747260313ec6c
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-03-09 13:20:09 +0530

    Time: 4 ms (70.24%), Space: 7.4 MB (89.25%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    void helper(set<vector<int>> &ans,vector<int> &curr,vector<int> &nums,int ind){
-        if(ind >= nums.size()){
-            ans.insert(curr);
-            return;
-        }
+    void helper(vector<vector<int>> &ans,vector<int> &curr,vector<int> &nums,int ind){
+        ans.push_back(curr);
         
-        helper(ans,curr,nums,ind + 1);
-        curr.push_back(nums[ind]);
-        helper(ans,curr,nums,ind + 1);
-        curr.pop_back();
+        for(int j=ind;j<nums.size();j++){
+            if(j > ind && nums[j] == nums[j-1]) continue;
+            curr.push_back(nums[j]);
+            helper(ans,curr,nums,j + 1);
+            curr.pop_back();
+        }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         
         vector<int> curr;
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         helper(ans,curr,nums,0);
-        vector<vector<int>> res;
-        for(auto e:ans){
-            res.push_back(e);
-        }
-        return res;
+        return ans;
     }
 };
