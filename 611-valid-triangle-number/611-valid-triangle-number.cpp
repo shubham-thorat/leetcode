7bacd5fa73e5e11ac249d6c8a756332a117dc779
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-04-21 13:26:23 +0530

    Time: 919 ms (8.52%), Space: 13.1 MB (5.25%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.cpp
+++ 611-valid-triangle-number/611-valid-triangle-number.cpp
@@ -4,16 +4,14 @@ public:
         int n = nums.size();
         sort(nums.begin(),nums.end());
         int count = 0;
-        for(int i=n-1;i>1;i--){
-            int l = 0,r = i-1;
-            while(l < r){
-                if(nums[l] + nums[r] > nums[i]) {
-                    count += (r-l);
-                    r--;
-                }
-                else {
-                    l++;
-                }
+        for(int i=0;i<n-2;i++) {
+            for(int j=i+1;j<n-1;j++) {
+                int idx = lower_bound(nums.begin() + j,nums.end(),nums[i] + nums[j]) - nums.begin();
+                
+                // if(idx != n) {
+                if((idx-j-1) >=0)
+                    count += (idx - j -1);
+                // }
             }
         }
         return count;
