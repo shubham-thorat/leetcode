a8adb601a09593f418e31be6359d9e6baf058a90
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-05-23 11:28:21 +0530

    Added solution - LeetHub

--- Count the triplets - GFG/count-the-triplets.java
+++ Count the triplets - GFG/count-the-triplets.java
@@ -29,6 +29,43 @@ class GFG {
 //User function Template for Java
 
 class Solution {
+    int lower_bound(int arr[],int lo,int hi,int val) {
+        int mid = -1;
+        while(lo < hi) {
+            mid = lo + (hi - lo)/2;
+            
+            if(arr[mid] < val) {
+                lo = mid + 1;
+            }
+            else  {
+                hi = mid;
+            }
+        }
+        
+        if(val <= arr[lo]) {
+            return lo;
+        }
+        if(val <= arr[hi]) {
+            return hi;
+        }
+        return -1;
+    }
+    
+    int upper_bound(int arr[],int lo,int hi,int val) {
+        while(lo < hi) {
+            int mid = lo + (hi - lo)/2;
+            if(arr[mid] <= val) {
+                lo = mid + 1;
+            }
+            else {
+                hi = mid;
+            }
+        }
+        
+        if(val < arr[lo]) return lo;
+        if(val < arr[hi]) return hi;
+        return hi + 1;
+  }
     int countTriplet(int arr[], int n) {
         // code here
         int count = 0;
@@ -36,14 +73,13 @@ class Solution {
         
         for(int i=0;i<n-2;i++) {
             for(int j=i+1;j<n-1;j++) {
-                for(int k=j+1;k<n;k++) {
-                    if(arr[i] + arr[j] == arr[k]) {
-                        count++;
-                    }
-                    else if(arr[i] + arr[j] < arr[k]) {
-                        break;
-                    }
+                int leftIdx =  lower_bound(arr,j+1,n-1,arr[i] + arr[j]);
+                if(leftIdx == -1 || arr[leftIdx] != arr[i] + arr[j]) {
+                    continue;
                 }
+                int rightIdx = upper_bound(arr,leftIdx,n-1,arr[i] + arr[j]);
+                // System.out.println(arr[i] + " " + arr[j] + " " +  leftIdx + " " + rightIdx);
+                count += rightIdx - leftIdx;
             }
         }
         return count;
