9c533eace640983210f96931d84d7808d797e2e7
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-05-31 22:59:06 +0530

    Time: 1073 ms (19.18%), Space: 71.7 MB (48.11%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,18 +1,7 @@
-typedef long long ll;
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        int n = ((1<<k));
-//        ll sum = (n * (n+1))/2;
-//         unordered_set<ll> st;
-        
-//         int curr_num = 0;
-//         for(int i=0;i<k-1;i++) {
-//             curr_num = curr_num 
-//         }
-//         for(int i=0;i<s.length();i++) {
-            
-//         }
+        int n = (1<<k);         
         if(k > s.length()) return 0;
         
         unordered_set<string> st;
@@ -24,20 +13,3 @@ public:
         return (st.size() == n);
     }
 };
-
-// "01" "00" "10" "11"
-
-// 00 
-// 01 
-// 10 
-// 11
-    
-// n = 2^2 - 1
-//   n * (n + 1)/2;
-
-// umap => 0,1,3,2
-
-// sum = 0 + 1 + 3 +  2
-    
-    
-// "00110110"
