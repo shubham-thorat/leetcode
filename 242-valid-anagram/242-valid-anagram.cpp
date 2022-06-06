afbe432cd90e3b115e9da082a68bab3e795e7d4a
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-06-06 18:18:52 +0530

    Time: 3 ms (98.42%), Space: 7.3 MB (46.61%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        if(s.length() != t.length()) return false;
         
-        return (s == t);
+        int charcount[26] = {0};
+        
+        for(char ch:s) {
+            charcount[ch - 'a'] += 1;
+        }
+        
+        for(char ch:t) {
+            charcount[ch - 'a'] -= 1;
+            
+            if(charcount[ch-'a'] < 0) return false;
+        }
+        return true;
     }
 };
