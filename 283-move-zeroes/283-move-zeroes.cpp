ea032440fcd2030ec9f74a14a1863fba2a6789af
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-06-06 18:20:38 +0530

    Time: 53 ms (17.39%), Space: 19.3 MB (14.34%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int prevNonZero = 0;
+        
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i] != 0) {
+                swap(nums[prevNonZero],nums[i]);
+                prevNonZero += 1;
+            }
+        }
+        
+        for(int i=prevNonZero + 1;i<nums.size();i++) {
+            nums[i] = 0;
+        }
+        
+    }
+};
