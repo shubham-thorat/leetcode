0b7c3f9408902e2e82c81fb28afea7b188196c59
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-07-01 21:43:12 +0530

    Time: 239 ms (41.32%), Space: 16.5 MB (81.09%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int solve(vector<int> &nums,int idx,vector<int> &dp){
-        
-        if(idx>=nums.size()-1) return 0;
-        if(dp[idx] != -1) return dp[idx];
-        if(nums[idx]==0) return dp[idx] = 1e5; //Not possible
-        else{
-            int ans = 1e5;
-            for(int i=1;i<=nums[idx];i++){
-                int curr = 1+solve(nums,idx+i,dp);
-                ans = min(curr,ans);
-            }
-            return dp[idx] = ans; //after exploring all the possibilities if not possible
-        }
-    }
     int jump(vector<int>& nums) {
-        vector<int> dp(nums.size() + 1,-1);
-        return solve(nums,0,dp);
+        int mn = 0,mx = 0;
+        int n = nums.size();
+        
+        int step = 0;
+        while(mx < (n-1)) {
+            
+            for(int i=0;i<n;i++) {
+                if((i + nums[i] > mx) && i <= mn) {
+                    mx = i + nums[i];
+                }
+            }
+            
+            step++;
+            mn = mx;
+        }
+        return step;
     }
 };
