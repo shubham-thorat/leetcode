73d00a0a9198ec2e5c0037b72a78bf5326c56641
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-07-01 21:31:24 +0530

    Time: 134 ms (9.52%), Space: 14.1 MB (95.15%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -4,13 +4,13 @@ public:
         int mn = 0,mx = 0;
         int open = 0;
         while(mx < n) {
-            int org = mx;
             for(int i=0;i<ranges.size();i++) {
                 if((i - ranges[i]) <= mn && (i+ranges[i]) > mx) {
                     mx = i + ranges[i];
                 }
             }
-            if(mx == org) return -1;
+            
+            if(mn == mx) return -1;
             
             open++;
             mn = mx;
