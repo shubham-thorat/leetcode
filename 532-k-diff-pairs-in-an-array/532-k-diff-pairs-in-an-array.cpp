c5605f0f458d5ae317e683461e6d5401e9a9fa05
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-07-01 17:55:12 +0530

    Time: 41 ms (25.95%), Space: 13.2 MB (53.48%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
-    void Insert(set<pair<int,int>> &st,int a,int b) {
-        st.insert({min(a,b),max(a,b)});    
-    }
-    int findPairs(vector<int>& nums, int k) {
-    
-        set<pair<int,int>> st;
+    int solve(vector<int> &nums) {
         unordered_map<int,int> mp;
         
-        for(auto &e:nums) {
-            if(mp.find(e - k) != mp.end()) {
-                Insert(st,e,e-k);
-            }
-            
-            if(mp.find(e + k) != mp.end()) {
-                Insert(st,e,e + k);
-            }
-            mp[e] += 1;
+        for(auto &e:nums) mp[e]++;
+        int ans = 0;
+        for(auto &e:mp) {
+            if(e.second > 1) ans++;
         }
-        return st.size();
+        return ans;
+    }
+    int findPairs(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        if(n <= 1) return 0;
+        
+        
+        if(k == 0) {
+            return solve(nums);
+        }
+        
+        unordered_set<int> st;
+        
+        
+        
+        st.insert(nums[0]);
+        int ans = 0;
+        for(int i=1;i<n;i++) {
+            if(nums[i] == nums[i-1]) continue;
+            
+            if(st.find(nums[i] - k) != st.end()) {
+                ans += 1;
+            }
+            st.insert(nums[i]);
+        }
+    
+        return  ans;
     }
 };
