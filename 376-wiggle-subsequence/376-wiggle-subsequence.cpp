a9939da3afa8c98e4d239f73065abcb124be25e4
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-07-03 19:09:37 +0530

    Time: 3 ms (69.26%), Space: 7.1 MB (75.45%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -6,22 +6,17 @@ public:
         if(n == 0) return 0;
         if(n == 1) return 1;
         
-        vector<int> negative(n,1),positive(n,1);
-        int ans = 1;
+        int peak = 1,valley = 1;
         for(int i=1;i<n;i++) {
-            for(int j=0;j<i;j++) {
-                int val = nums[i] - nums[j];
-                
-                if(val > 0) {
-                    positive[i] = max(positive[i],negative[j] + 1);
-                }
-                else if(val < 0){
-                    negative[i] = max(negative[i],positive[j] + 1);
-                }
+            
+            if(nums[i] > nums[i-1]) {
+               peak = valley + 1;
             }
-            // cout << positive[i] << " " << negative[i] << endl;
-            ans = max({ans,positive[i],negative[i]});
+            
+            if(nums[i] < nums[i-1]) {
+              valley = peak + 1;
+            }
         }
-        return ans;
+        return max(peak,valley);
     }
 };
