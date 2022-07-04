0c59d8f9c87e70e9005e8a980d7b3f59c7bc2d64
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-07-04 19:49:36 +0530

    Time: 15 ms (31.58%), Space: 13.7 MB (71.75%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,19 +1,51 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n = nums.size();
+        int sizeNums = nums.size();
         
-        int ans = INT_MIN;
-        int lprod = 1,rprod = 1;
-        for(int i=0;i<n;i++) {
-            lprod = lprod * nums[i];
-            rprod = rprod * nums[n-i-1];
-            ans = max({ans,lprod,rprod});
+        int maxProduct = INT_MIN;
+//         for(int i=0;i<sizeNums;i++) {
+//             int currentProduct = 1;
+//             for(int j=i;j<sizeNums;j++) {
+//                 currentProduct = currentProduct * nums[j];
+                
+//                 maxProduct = max(maxProduct,currentProduct);
+//             }
+//         }
+//         return maxProduct;
+        
+        int currentProduct = 1;
+        for(int i=0;i<sizeNums;i++) {
+            currentProduct = currentProduct * nums[i];
             
-            if(lprod == 0) lprod = 1;
-            if(rprod == 0) rprod = 1;
+            maxProduct = max(maxProduct,currentProduct);
+            
+            if(currentProduct == 0) {
+                currentProduct = 1;
+            }
         }
         
-        return ans;
+        currentProduct = 1;
+        for(int i=sizeNums-1;i>=0;i--) {
+            currentProduct = currentProduct * nums[i];
+            
+            maxProduct = max(maxProduct,currentProduct);
+            
+            if(currentProduct == 0) {
+                currentProduct = 1;
+            }
+        }
+        
+        return maxProduct;
     }
 };
+
+// O(n^2)
+// O(1)
+
+
+// [2,3,-2,4]
+
+//claim -> assumption ->
+
+// maxProduct =
