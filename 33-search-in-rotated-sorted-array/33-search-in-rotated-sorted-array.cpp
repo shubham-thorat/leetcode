f56d63ee582604332de9688b471f8df0db00019f
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-07-06 13:37:04 +0530

    Time: 0 ms (100.00%), Space: 11.2 MB (29.13%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,33 +1,50 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        
+    int findPivot(vector<int> nums) {
         
         int low = 0,high = nums.size() - 1;
         
         while(low <= high) {
-            int mid = low + (high - low)/2;
+            int mid = (low + high)>>1;
             
-            if(nums[mid] == target) return mid;
-            
-            if(nums[low] <= nums[mid]) {
-                if(nums[low] <= target && nums[mid] > target) {
-                    high = mid - 1; 
-                }
-                else {
-                    low = mid + 1;
-                }
+            if(mid < high && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            else if(low < mid && nums[mid - 1] > nums[mid]) {
+                return mid - 1;
+            }
+            else if(nums[low] >= nums[mid]) {
+                high = mid - 1;
             }
             else {
-                if(nums[mid] < target && nums[high] >= target) {
-                    low = mid + 1; 
-                }
-                else {
-                    high = mid - 1;
-                }
+                low = mid + 1;
             }
         }
-        
         return -1;
+    }
+    int search(vector<int> &nums,int val,int low,int high) {
+         // int low = 0,high = nums.size() - 1;
+        
+        while(low <= high) {
+            int mid = (low + high)>>1;
+            
+            if(nums[mid] == val) return mid;
+            
+            if(nums[mid] > val) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        
+        int pidx = findPivot(nums);
+        int l = search(nums,target,0,pidx);
+        if(l != -1) return l;
+        int r = search(nums,target,pidx + 1,nums.size() - 1);
+        return r;
     }
 };
