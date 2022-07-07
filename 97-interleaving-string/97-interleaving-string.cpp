87ade1f7aa661625f194a79471e2d3b7451b7032
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-07-07 12:15:06 +0530

    Time: 43 ms (26.14%), Space: 31.4 MB (17.60%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
+    vector<vector<int>> dp;
     bool solve(string s1,string s2,string s3,int i,int j,int k) {
         
         if(k == s3.length()) {
@@ -9,35 +9,33 @@ public:
         int size1 = s1.length();
         int size2 = s2.length();
         int size3 = s3.length();
-        if(dp[i][j][k] != -1) {
-            return dp[i][j][k];
+        if(dp[i][j] != -1) {
+            return dp[i][j];
         }
         
         if((size1 - i) + (size2 - j) != (size3 - k)) {
-            return dp[i][j][k] = false;
+            return dp[i][j] = false;
         }
         if(i < size1 && j < size2 && s1[i] == s2[j]) {
             if(s1[i] == s3[k]) {
-                return dp[i][j][k] = solve(s1,s2,s3,i+1,j,k + 1) || solve(s1,s2,s3,i,j+1,k + 1);  
+                return dp[i][j] = solve(s1,s2,s3,i+1,j,k + 1) || solve(s1,s2,s3,i,j+1,k + 1);  
             }
             else {
-                return dp[i][j][k] = false;
+                return dp[i][j] = false;
             }
         }
         else if(i < size1 && s1[i] == s3[k]) {
-            return dp[i][j][k] = solve(s1,s2,s3,i+1,j,k+1);
+            return dp[i][j] = solve(s1,s2,s3,i+1,j,k+1);
         }
         else if(j < size2 && s2[j] == s3[k]) {
-            return dp[i][j][k] = solve(s1,s2,s3,i,j+1,k+1);
+            return dp[i][j] = solve(s1,s2,s3,i,j+1,k+1);
         }
         else {
-            return dp[i][j][k] = false;
+            return dp[i][j] = false;
         }
     }
     bool isInterleave(string s1, string s2, string s3) {
-        dp.resize(101,vector<vector<int>>(101,vector<int>(201,-1)));
-    
-        // vector<vector<vector<int>>> dp(101,vector<vector<int>>(101,vector<int>(201,-1)));
+        dp.resize(101,vector<int>(101,-1));
         return solve(s1,s2,s3,0,0,0) || solve(s2,s1,s3,0,0,0);        
     }
 };
