7996d29ef75ed04e8c0455ce6019a592e57da05d
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2022-07-24 14:05:01 +0530

    Time: 130 ms (59.13%), Space: 17.2 MB (64.90%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     static bool mysort(string &a,string &b) {
-        return a.length() < b.length();
+        return a.length() > b.length();
     }
     int helper(string word,unordered_set<string> &dict) {
         int ans = 0;
         dict.erase(word);
         for(int i=0;i<=word.length();i++) {
-            
-            for(char ch='a';ch<='z';ch++) {
-                string str = word.substr(0,i) + ch + word.substr(i);
-                if(dict.find(str) != dict.end()) {
-                    int temp = helper(str,dict) + 1;
-                    ans = max(ans,temp);
-                }
+            string str = word.substr(0,i) +  word.substr(min((int)word.length(),i + 1));
+            if(dict.find(str) != dict.end()) {
+                int temp = helper(str,dict) + 1;
+                ans = max(ans,temp);
             }
         }
         return ans;
     }
     int longestStrChain(vector<string>& words) {
         sort(words.begin(),words.end(),mysort);
-       unordered_set<string> dict;
+        unordered_set<string> dict;
         
         for(auto &e:words) {
             dict.insert(e);
